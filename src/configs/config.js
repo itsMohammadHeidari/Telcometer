ba58c7b6a5652eca2f5dfd980903f418ddad5368
Mohammad Heidari <itsMohammadHeidari@gmail.com>
2024-04-01 23:21:49 +0330

    feat: add general config

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,1 +1,21 @@
-//
+import { sleep } from 'k6';
+import { SharedArray } from 'k6/data';
+sleep(1);
+
+export const cfg = new SharedArray("constant data", function () {
+    return [
+        {
+            get: {
+                MCC: "418",
+                MNC: "020",
+                TAC: "35684610", // it used to generate random IMEISVs
+                numberOfAccounts: 5,
+                diamClient: "localhost:3868",
+                downloadedBytes: 2621424,
+                uploadedBytes: 2621424,
+                BitrateUL: 1073741000,
+                BitrateDL: 1073741000
+            }
+        }
+    ]
+});
