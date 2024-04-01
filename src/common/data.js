d2510188427d5be423965d946f7a2f67c958d629
Mohammad Heidari <itsMohammadHeidari@gmail.com>
2024-04-01 23:34:17 +0330

    feat: add SharedArray for diameter command codes

--- src/common/data.js
+++ src/common/data.js
@@ -1,1 +1,11 @@
-//
+import { SharedArray } from 'k6/data';
+
+export const cmd = new SharedArray("diameter commands", function () {
+    return [
+        {
+            get: {
+                CreditControl: 272
+            }
+        }
+    ]
+});
