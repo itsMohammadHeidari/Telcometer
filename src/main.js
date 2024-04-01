f8e71fbbb475bf386cd1f72a88e8fc8c56fe826e
Mohammad Heidari <itsMohammadHeidari@gmail.com>
2024-04-01 23:25:00 +0330

    feat: add diameter client

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-//
+import diam from 'k6/x/diameter';
+
+let client = diam.Client({
+    // Timeout for each request
+    requestTimeout: "100ms",
+
+    // Flag to enable automatic DWR (Diameter Watchdog Request)
+    enableWatchdog: false,
+
+    // List of authentication application IDs
+    authApplicationId: [4],
+
+    // List of vendor-specific application IDs
+    vendorSpecificApplicationId: [
+        {
+            authApplicationId: 4,
+            vendorId: 10415,
+        }
+    ],
+    capabilityExchange: {
+        vendorId: 35838,
+        OriginHost: "epc.mnc020.mcc418.3gppnetwork.org"
+    },
+});
